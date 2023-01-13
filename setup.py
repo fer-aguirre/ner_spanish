93c5944cbaf9121eb63500f393a56e070cb2abe7
Fer Aguirre <kay_op@protonmail.com>
2023-01-12 20:45:30 -0600

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='NER Spanish',
+    version='0.1.0',
+    author='Fer Aguirre',
+    description='A repository for extracting Named Entity Recognition (NER) in Spanish data.',
+    python_requires='>=3',
+    license='MIT License',
+    packages=find_packages(),
+)
